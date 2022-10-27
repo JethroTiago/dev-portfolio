f310c92d9fd99ee939d596a0cd257c5977a819a7
JETHRO TIAGO <103612874+JethroTiago@users.noreply.github.com>
2022-10-26 23:04:10 -0300

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,2 +1,3 @@
 console.log('Projeto desenvolvido por Daniel Jordão e Jethro Tiago');
+Console.log('Este projeto foi baseado no projeto Freelancer - Start Bootstrap - https://startbootstrap.github.io/startbootstrap-freelancer/')
 console.log('Outubro - 2022');
